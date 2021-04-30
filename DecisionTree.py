6abe1982fb6f82a13670edb8f1df0ed8ddb3f9ff
GagandeepBatra <coke0926@hotmail.com>
2021-04-30 15:41:43 -0400

    Initial commit

---
+++ DecisionTree.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from sklearn import tree, metrics, model_selection
+
+# load data set
+header = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class']
+data = pd.read_csv('car.data', names=header)
+
+# target variable
+data['class'], class_names = pd.factorize(data['class'])
+
+# identify predictor variables to integer codes
+data['buying'],_ = pd.factorize(data['buying'])
+data['maint'],_ = pd.factorize(data['maint'])
+data['doors'],_ = pd.factorize(data['doors'])
+data['persons'],_ = pd.factorize(data['persons'])
+data['lug_boot'],_ = pd.factorize(data['lug_boot'])
+data['safety'],_ = pd.factorize(data['safety'])
+
+# select predictor and target
+x = data.iloc[:, :-1]
+y = data.iloc[:, -1]
+
+# train test splits
+x_train_90, x_test_10, y_train_90, y_test_10 = model_selection.train_test_split(x, y, test_size=0.1, random_state=0)
+x_train_60, x_test_40, y_train_60, y_test_40 = model_selection.train_test_split(x, y, test_size=0.4, random_state=0)
+x_train_50, x_test_50, y_train_50, y_test_50 = model_selection.train_test_split(x, y, test_size=0.5, random_state=0)
+
+# train the decision tree
+dtree = tree.DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)
+
+# results 90/10 split
+dtree.fit(x_train_90, y_train_90)
+y_pred_10 = dtree.predict(x_test_10)
+count_misclassified = (y_test_10 != y_pred_10).sum()
+print('90/10 Split')
+print('Misclassified samples: {}'.format(count_misclassified))
+accuracy = metrics.accuracy_score(y_test_10, y_pred_10)
+print('Accuracy: {:.2f}'.format(accuracy))
+
+# results 60/40 split
+dtree.fit(x_train_60, y_train_60)
+y_pred_40 = dtree.predict(x_test_40)
+count_misclassified = (y_test_40 != y_pred_40).sum()
+print('60/40 Split')
+print('Misclassified samples: {}'.format(count_misclassified))
+accuracy = metrics.accuracy_score(y_test_40, y_pred_40)
+print('Accuracy: {:.2f}'.format(accuracy))
+
+# results 50/50 split
+dtree.fit(x_train_50, y_train_50)
+y_pred_50 = dtree.predict(x_test_50)
+count_misclassified = (y_test_50 != y_pred_50).sum()
+print('50/50 Split')
+print('Misclassified samples: {}'.format(count_misclassified))
+accuracy = metrics.accuracy_score(y_test_50, y_pred_50)
+print('Accuracy: {:.2f}'.format(accuracy))
